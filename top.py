b0b1dfd43f7374b920d43ebcd2794737769c42dd
Tim Blankers <timblankers@gmail.com>
2013-09-19 11:05:50 +0200

    Adding top.py

---
+++ top.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import argparse
+import operator
+import subprocess
+import glob
+import os
+
+def inverse(x):
+    return 1.0 / x
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_date")
+    args = parser.parse_args()
+    print("Analyzing", args.input_date)
+    
+    total = {}
+    announcements = {}
+    withdrawals = {}
+    date = args.input_date
+    show = 5
+    
+    print(">> Parsing updates")
+    for update_file in glob.glob(date+"/*.bz2"):
+        print(update_file+".parsed")
+        # Check if file is already parsed
+        try:
+            with open(update_file+".parsed"): pass
+        except IOError:
+            subprocess.call(["bgpdump", "-m", update_file, "-O", update_file+".parsed"])
+        input_file = open(update_file+".parsed", "r").read()
+
+        # Count updates in file
+        for message in input_file.splitlines():
+            fields = message.strip().split("|")
+            if fields[4] in total:
+                total[fields[4]] += 1
+            else:
+                total[fields[4]] = 1
+            if fields[2] == "A":
+                if fields[4] in announcements:
+                    announcements[fields[4]] += 1
+                else:
+                    announcements[fields[4]] = 1
+            elif fields[2] == "W":
+                if fields[4] in withdrawals:
+                    withdrawals[fields[4]] += 1
+                else:
+                    withdrawals[fields[4]] = 1
+    print(">> Parsing finished")
+    
+    top_talkers = sorted(total.items(), key=lambda x:x[1], reverse=True)
+    top_announcements = sorted(announcements.items(), key=lambda x:x[1], reverse=True)
+    top_withdrawals = sorted(withdrawals.items(), key=lambda x:x[1], reverse=True)
+    
+    print("Top", show, "Talkers")
+    for x in range(0, show):
+        print(x+1, "AS:", top_talkers[x][0], "Total:", top_talkers[x][1])
+    print("Top", show, "Announcements")
+    for x in range(0, show):
+        print(x+1, "AS:", top_announcements[x][0], "Total:", top_announcements[x][1])
+    print("Top", show, "Withdrawals")
+    for x in range(0, show) :
+        print(x+1, "AS:", top_withdrawals[x][0], "Total:", top_withdrawals[x][1])
+    
+if __name__ == '__main__':
+    main()
