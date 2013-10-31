97c010366e2f7eb48de54f4d1b01f55a2032ca14
Tim Blankers <timblankers@gmail.com>
2013-10-31 11:12:27 +0100

    Stockholm only

--- get_month.sh
+++ get_month.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-CITY="oregon"
+CITY="stockholm"
 YEAR="2013"
-MONTH="05"
+MONTH="02"
 mkdir ${CITY}
 cd ${CITY}
 mkdir ${YEAR}
 cd ${YEAR}
 mkdir ${MONTH}
 cd ${MONTH}
-for DAY in {29..30}
+for DAY in {01..30}
 do
     echo ">> Retrieving day ${DAY}"
     mkdir ${YEAR}${MONTH}${DAY}
     cd ${YEAR}${MONTH}${DAY}
-    curl --limit-rate 400K --fail -O http://data.ris.ripe.net/rrc15/${YEAR}.${MONTH}/updates.${YEAR}${MONTH}${DAY}.[00-23][00-55:5].gz
-    #curl --limit-rate 400K --fail -O http://archive.routeviews.org/bgpdata/${YEAR}.${MONTH}/UPDATES/updates.${YEAR}${MONTH}${DAY}.[00-23][00-55:15].bz2
+    for HOUR in {00..23}
+    do
+        for MINUTE in {00..55..5}
+        do
+            curl --limit-rate 400K --fail -O http://data.ris.ripe.net/rrc07/${YEAR}.${MONTH}/updates.${YEAR}${MONTH}${DAY}.${HOUR}${MINUTE}.gz
+        done
+    done
     cd ..
 done
