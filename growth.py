0235410bccf17ec0804c555a7470a59c7ab079e1
Tim Blankers <timblankers@gmail.com>
2013-11-12 16:01:54 +0100

    remove prefix instability

--- growth.py
+++ growth.py
@@ -21,7 +21,6 @@ def main():
     enc='iso-8859-15'
     
     counter = "prefix" # origin_announcement prefix as_path
-    unstable_prefixes = True
     
     file_counter = 0
     directories = []
@@ -50,12 +49,8 @@ def main():
                         if fields[0] == counter:
                             if directory == directories[0]:
                                 data_start[fields[1]] += int(fields[2])
-                                if unstable_prefixes:
-                                    unstable_prefixes_start[update_file].append((fields[1]))
                             elif directory == directories[1]:
                                 data_end[fields[1]] += int(fields[2])
-                                if unstable_prefixes:
-                                    unstable_prefixes_end[update_file].append((fields[1]))
                     input_file.close()
 
     for key, value in data_start.items():
@@ -74,24 +69,6 @@ def main():
     for x in range(len(sorted_differences)-10, len(sorted_differences)):
         percentage = data_end[sorted_differences[x][0]]*(100/data_start[sorted_differences[x][0]])
         print("%d: %s (%s, %.2f%%) " % (x+1, sorted_differences[x][0], sorted_differences[x][1], percentage))
-    
-    print(">> Unstable prefixes per day in start:")
-    total_unpref_start = 0
-    total_start = 0
-    for x in unstable_prefixes_start:
-        print("%d" % len(unstable_prefixes_start[x]), end=', ')
-        total_unpref_start += len(unstable_prefixes_start[x])
-        total_start += 1
-    print(">> Average prefixes/day start: %d" % (total_unpref_start/total_start))
-
-    print(">> Unstable prefixes per day in end:")
-    total_unpref_end = 0
-    total_end = 0
-    for x in unstable_prefixes_end:
-        print("%d" % len(unstable_prefixes_end[x]), end=', ')
-        total_unpref_end += len(unstable_prefixes_end[x])
-        total_end += 1
-    print(">> Average prefixes/day end:   %d" % (total_unpref_end/total_end))
     
     only_start = 0
     updates_start = 0
